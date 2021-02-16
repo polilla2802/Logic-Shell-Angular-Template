27f875bf11ca6bce85b85e0270051ad679bbcb51
Jose Manuel Alarcon <140300080@ucaribe.edu.mx>
2021-02-15 21:52:28 -0500

    changed layout and logo to template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    MatSliderModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

5f5c8abe3cf4129ab97d69f2f598ff29c29ec114
polilla2802 <polilla2802@gmail.com>
2021-02-19 13:49:04 -0500

    basics typescript

--- src/app/algos/basics.ts
+++ src/app/algos/basics.ts
@@ -1,3 +1,4 @@
+// Typescript typed function
 export function Evolve(level: number, legendary: boolean): string {
     if (legendary == true) {
         return "legendaries don't evolve!"
@@ -10,42 +11,44 @@ export function Evolve(level: number, legendary: boolean): string {
     }
 }
 
-export function Basics():void{
+export function Basics(): void {
 
+    // Typescript interface
     interface Pokemon {
-        name:string,
-        type:string,
-        level:number
-      }
-      
-      const arr:Pokemon[] = [];
-      
-      const pokemon1 = {
-        name:"latios",
-        type:"dragon",
-        level:42
-      }
-      
-      const pokemon2 = {
-        name:"latias",
-        type:"dragon",
-        level:40
-      }
-      
-      arr.push(pokemon1);
-      arr.push(pokemon2);
-      
-      // console.log(arr);
-      
-      type MyPokemon = [string, string, number, boolean]
-      
-      type Style = string;
-      
-      const arr2:MyPokemon = ["latios", "latias", 36, false];
-      
-      console.log(arr2);
-      
-      console.log(Evolve(36,false));
+        name: string,
+        type: string,
+        level: number
+    }
+
+    // Typescript array of objects
+    const arr: Pokemon[] = [];
+
+    // Typescript objects
+    const pokemon1 = {
+        name: "latios",
+        type: "dragon",
+        level: 42
+    }
+
+    const pokemon2 = {
+        name: "latias",
+        type: "dragon",
+        level: 40
+    }
+
+    arr.push(pokemon1);
+    arr.push(pokemon2);
+
+    // console.log(arr);
+
+    // Typescript tuple
+    type MyPokemon = [string, string, number, boolean]
+
+    const arr2: MyPokemon = ["latios", "latias", 36, false];
+
+    console.log(arr2);
+
+    console.log(Evolve(36, false));
 }
 
 

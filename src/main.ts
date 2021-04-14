0bf0ac0a7acfda59fa1aac24d7538bb5acb8137d
polilla2802 <polilla2802@gmail.com>
2021-04-14 12:59:12 -0400

    created help folder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Basics, Evolve } from './assets/ts/basics';
+import { Basics, Evolve } from './assets/help/ts/basics';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
